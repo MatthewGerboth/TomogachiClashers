5dda1f1fb04a3b379ba492b7a76eee94e275acf8
AlexN572 <anewlands572@gmail.com>
2025-12-10 10:41:32 -0800

    Just checking

---
+++ battleHealer.java
@@ -0,0 +1,27 @@
+public class battleHealer {
+    
+    private String name;
+    private int health;
+    private int damage;
+    private int happiness;
+    private int level;
+    private boolean evolve;
+    private int cards;
+    private int maxHealth;
+
+    public battleHealer(String name){
+        this.name = name;
+        health = 100;
+        damage = 100;
+        happiness = 100;
+        level = 1;
+        evolve = false;
+        cards = 0;
+    }
+
+    public void eat(){
+        System.out.println("Your battle healer has gotten something to eat. They have regained 20 health.");
+        private
+    }
+
+}
--- interface.java
+++ interface.java
@@ -3,6 +3,5 @@ interface Card {
     public void eat();
     public void levelUp();
     public void evolve();
-    public void attack();
     public void placeCard();
 }
