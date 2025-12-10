d52800ee5150edf62d283dee95e69ba1a721c0fb
BrandonPhung <bphung0324@gmail.com>
2025-12-10 10:38:45 -0800

    skelleton class

--- skelleton.java
+++ skelleton.java
@@ -2,7 +2,6 @@ public class skelleton{
     //Instance variables 
         private int level;
         private int damage;
-        private int foodPreference;
         private boolean evo;
         private int health;
         private int happiness;
@@ -10,28 +9,11 @@ public class skelleton{
        
     
        // Default constructor
-        public Skelleton() {
+        public skelleton() {
             this.level = 1;
             this.health = 100;
             this.damage = 50;
             this.evo = true;
-            this.foodPreference = "Milk";
-    
-        }
-    
-        // Constructor with name parameter
-        public Skelleton(String name) {
-            this.level = 1;
-            this.health = 100;
-            this.damage = 50;
-            this.evo = true;
-            this.foodPreference = "Milk";
-        }
-    //Method to simulate a fight
-        public void fight() {
-            int damageDealt = 50;
-            int damageTaken = 30;
-            health -= damageTaken;
         }
     //Method to eat and restore health
         public void eat() {
@@ -39,10 +21,6 @@ public class skelleton{
             if (this.health > 100) {
                 this.health = 100; // Cap health at 100
             }
-        }
-    //Method to get food preference
-        public String getFoodPreference() {
-            return foodPreference;
         }
     // Method to level up
         public void levelUp() {
@@ -52,4 +30,3 @@ public class skelleton{
             this.happiness += 15; // Increase happiness by 15
         }
     }
-    
